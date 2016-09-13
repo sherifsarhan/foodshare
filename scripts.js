10ca9778655b07a33bfd051defd122a49756c5ce
Sherif <sherif.a.sarhan@gmail.com>
2016-09-12 23:23:28 -0400

    Added Username, password, login, and remember me elements.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
+require([
+    "esri/Map",
+    "esri/views/MapView",
+    "dojo/domReady!"
+], function(Map, MapView) {
 
+    var map = new Map({
+        basemap: "streets"
+    });
+
+    var view = new MapView({
+        container: "viewDiv",
+        map: map,
+        zoom: 5,
+        center: [-99, 39]
+    });
+
+});
