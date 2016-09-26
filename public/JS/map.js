b341f85393174c0b43396f3e01532781d6f10e74
Sherif <sherif.a.sarhan@gmail.com>
2016-09-26 04:30:13 -0400

    clicking on markers will update the location as well as the input text. future will allow for editing marker text.

--- public/JS/map.js
+++ public/JS/map.js
@@ -10,20 +10,21 @@ var config = {
 firebase.initializeApp(config);
 
 var foodshareRef = firebase.database().ref("foodshare");
+
 var submitClicked = false;
-var markerCountStr;
 var latLng;
-var marker;
+var markers = [];
+var infoWindows = [];
+var map;
+var count=0;
 //--------------GOOGLE MAPS-----------------
 function initMap() {
     var mapDiv = document.getElementById('map');
-    var map = new google.maps.Map(mapDiv, {
+    map = new google.maps.Map(mapDiv, {
         center: {lat: 38.8320, lng: -77.3116},
         zoom: 16
     });
 
-    var markerCount = 0;
-    var markers = [];
     var prevMarker;
     var firstRun = true;
     map.addListener('click', function(e) {
@@ -31,42 +32,67 @@ function initMap() {
 
         if(!submitClicked && !firstRun){
             prevMarker.setMap(null);
-            markerCount--;
+            count--;
         }
         submitClicked = false;
         firstRun = false;
 
         latLng = e.latLng;
-        marker = addMarker(latLng, map);
-        markers.push(marker);
+        var marker = addMarker(latLng, map, "");
+        console.log(marker.id);
+        console.log(marker.text);
         $('#lat').text(latLng.lat());
         $('#lng').text(latLng.lng());
-        // foodshareRef.push({'lat' : latLng.lat(), 'lng' : latLng.lng()});
 
         prevMarker = marker;
+
+        // marker.addListener('click', function () {
+        //     // open the infowindow for the corresponding marker
+        //     infoWindow.open(marker.get('map'), marker);
+        // });
     });
 
     // adds a new marker to the map
-    function addMarker(latLng, map) {
-        markerCount++;
-        markerCountStr = markerCount.toString();
+    function addMarker(latLng, map, text) {
         var marker = new google.maps.Marker({
             position: latLng,
-            map: map
+            map: map,
+            id: count,
+            text: text
         });
 
+        count++;
         return marker;
     }
-    google.maps.event.addDomListener(window, "load", initMap);
 
-}
+    foodshareRef.on("child_added", function(data){
+        myLatLng = {lat: data.val().lat, lng: data.val().lng};
+        var tempMarker = addMarker(myLatLng, map, data.val().food);
 
+        // // creates the info window for the marker
+        var infoWindow = new google.maps.InfoWindow({
+            content: data.val().food
+        });
+        // open the infowindow to the corresponding marker
+        infoWindow.open(tempMarker.get('map'), tempMarker);
 
-function attachMessage(marker, message) {
-    var infowindow = new google.maps.InfoWindow({
-        content: message
+        tempMarker.addListener('click', function () {
+            // var marker = this;
+            LatLng = tempMarker.position;
+            $('#lat').text(LatLng.lat());
+            $('#lng').text(LatLng.lng());
+            $('.foodInfo').val(data.val().food);
+            infoWindow.open(tempMarker.get('map'), tempMarker);
+            console.log(tempMarker.id);
+        });
+
     });
+
+
+
+    google.maps.event.addDomListener(window, "load", initMap);
 }
+
 //--------------JS FUNCTIONS-----------------
 
 //Takes an ID, hides it if its currently on the page, returns bool on what happened.
@@ -119,13 +145,9 @@ $(document).ready(function() {
 
     $('.sbmtFood').on('click', function () {
         foodshareRef.push({'food' : $('.foodInfo').val(),'lat' : latLng.lat(), 'lng' : latLng.lng()});
-        // // creates the info window for the marker
-        var infowindow = new google.maps.InfoWindow({
-            content: $('.foodInfo').val()
-        });
-        // open the infowindow to the corresponding marker
-        infowindow.open(marker.get('map'), marker);
+
         submitClicked = true;
     });
+
 });
 
