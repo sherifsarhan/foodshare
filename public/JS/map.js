28430dee376a7a1927da5d8c12232150a3bd09e8
Sherif <sherif.a.sarhan@gmail.com>
2016-09-25 21:20:00 -0400

    allows flexible movements of google maps markers.  infobox now has a submit button and input. Marker can be moved around until "submit" is pressed

--- public/JS/map.js
+++ public/JS/map.js
@@ -1,4 +1,18 @@
+//--------------FIREBASE-----------------
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCtdy0Gf8tNWQC4bS6QcnH3X-vknhfY3R8",
+    authDomain: "foodshare-1474316972332.firebaseapp.com",
+    databaseURL: "https://foodshare-1474316972332.firebaseio.com",
+    storageBucket: "foodshare-1474316972332.appspot.com",
+    messagingSenderId: "151948214475"
+};
+firebase.initializeApp(config);
+
+var foodshareRef = firebase.database().ref("foodshare");
+
 //--------------GOOGLE MAPS-----------------
+var submitClicked = false;
 function initMap() {
     var mapDiv = document.getElementById('map');
     var map = new google.maps.Map(mapDiv, {
@@ -6,35 +20,54 @@ function initMap() {
         zoom: 16
     });
 
+    var markers = [];
+    var prevMarker;
+    var firstRun = true;
     map.addListener('click', function(e) {
-        placeMarkerAndPanTo(e.latLng, map);
+        if(!submitClicked && !firstRun){
+            prevMarker.setMap(null);
+        }
+        submitClicked = false;
+        firstRun = false;
+
+        var latLng = e.latLng;
+        var marker = addMarker(latLng, map);
+        // foodshareRef.push({'lat' : latLng.lat(), 'lng' : latLng.lng()});
+
+        prevMarker = marker;
     });
 
-    function placeMarkerAndPanTo(latLng, map) {
+    // adds a new marker to the map
+    function addMarker(latLng, map) {
         var marker = new google.maps.Marker({
             position: latLng,
             map: map
         });
 
+        // creates the info window for the marker
         var infowindow = new google.maps.InfoWindow({
-            content: '<div contentEditable="true">foasod</div>'
+            // content: '<div contentEditable="true">Add food info</div>'
+            content:
+            '<div class="infoClass">' +
+            '<input type="text" placeholder="Enter food info" required>' +
+            '<button onclick="submitAction()" class="sbmtFood" type="submit">Submit</button>' +
+            '</div>'
         });
 
+        // open the infowindow when a marker is added to the map
+        infowindow.open(marker.get('map'), marker);
+
+        // shows the info window if a marker gets clicked
         marker.addListener('click', function() {
             infowindow.open(marker.get('map'), marker);
         });
+        return marker;
+    }
+    google.maps.event.addDomListener(window, "load", initMap);
+}
 
-        infowindow.open(marker.get('map'), marker);
-    };
-
-    var marker = new google.maps.Marker({
-        position: {
-            lat: 38.8315,
-            lng: -77.3090
-        },
-        map: map
-    });
-    attachMessage(marker, "leftover chipotle");
+function submitAction() {
+    submitClicked = true;
 }
 
 function attachMessage(marker, message) {
