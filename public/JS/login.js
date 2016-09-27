2c920fa381af28615eaa330b77f23bc7a5ead09a
mboudrig <marwane23@gmail.com>
2016-09-26 22:38:23 -0400

    Problem with Firebase in login

--- public/JS/login.js
+++ public/JS/login.js
@@ -1,3 +1,17 @@
+//--------------FIREBASE-----------------
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCtdy0Gf8tNWQC4bS6QcnH3X-vknhfY3R8",
+  authDomain: "foodshare-1474316972332.firebaseapp.com",
+  databaseURL: "https://foodshare-1474316972332.firebaseio.com",
+  storageBucket: "foodshare-1474316972332.appspot.com",
+  messagingSenderId: "151948214475"
+};
+firebase.initializeApp(config);
+
+var foodshareRef = firebase.database().ref("foodshare");
+
+
 //--------------JS FUNCTIONS-----------------
 
 //Takes an ID, hides it if its currently on the page, returns bool on what happened.
@@ -12,19 +26,35 @@ function hideIfOnPage(hideID) {
 //-------------DOCUMENT READY----------------
 $( document ).ready(function() {
 
-    //hardcoded login validation.
-    $("#loginbtn").click(function() {
-        hideIfOnPage("#logsuccess");
-        hideIfOnPage("#logfail");
-        var uname = $("#uname");
-        console.log(uname.val());
-        if (uname.val() == "mboudrig" || uname.val() == "ssarhan2"){
-            $("<div id='logsuccess'>Welcome Back!</div>").insertAfter("#chkbox");
-        }
-        else{
-            $("<div id='logfail'>Access Denied.</div>").insertAfter("#chkbox");
-        }
+  $("#loginbtn").click(function() {
+    var ref = new Firebase("https://foodshare-1474316972332.firebaseio.com");
+    ref.authWithPassword({
+      email    : $("#uname").val(),
+      password : $("#pword").val()
+    }, function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+      }
     });
+  });
+
+  $("#regbtn").click(function() {
+    var ref = new Firebase("https://foodshare-1474316972332.firebaseio.com");
+    ref.createUser({
+      email    : $("#unamer").val(),
+      password : $("#pwordr").val()
+    }, function(error, userData) {
+      if (error) {
+        console.log("Error creating user:", error);
+      } else {
+        console.log("Successfully created user account with uid:", userData.uid);
+      }
+    });
+  });
+
+
 
     //For the LOGIN FORM AND FOR HISTORY MANIPULATION
 // Get the modal
@@ -43,5 +73,20 @@ $( document ).ready(function() {
         document.getElementById('id01').style.display = "none";
     });
 
-});
+  // Get the modal
+  var modal2 = document.getElementById('id02');
 
+// When the user clicks anywhere outside of the modal, close it
+  document.onclick = function(event) {
+    //have the state ready for when the user wants to click back
+    history.pushState(null, null, "login.html");
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  window.addEventListener('popstate', function(e) {
+    document.getElementById('id02').style.display = "none";
+  });
+
+});
