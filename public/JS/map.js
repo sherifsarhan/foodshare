0312cdb1b202655492db05968422990402c1c88c
Sherif <sherif.a.sarhan@gmail.com>
2016-10-29 21:04:21 -0400

    added ability to center map on current location

--- public/JS/map.js
+++ public/JS/map.js
@@ -36,13 +36,76 @@ var markers = {};
 var map;
 var count=0;
 var pointerMarker;
+var pos;
 //--------------GOOGLE MAPS-----------------
+/**
+ * The CenterControl adds a control to the map that recenters the map on
+ * Chicago.
+ * This constructor takes the control DIV as an argument.
+ * @constructor
+ */
+function CenterControl(controlDiv, map) {
+
+    // Set CSS for the control border.
+    var controlUI = document.createElement('div');
+    controlUI.style.backgroundColor = '#fff';
+    controlUI.style.border = '2px solid #fff';
+    controlUI.style.borderRadius = '3px';
+    controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.marginBottom = '22px';
+    controlUI.style.textAlign = 'center';
+    controlUI.title = 'Click to recenter the map';
+    controlDiv.appendChild(controlUI);
+
+    // Set CSS for the control interior.
+    var controlText = document.createElement('div');
+    controlText.style.color = 'rgb(25,25,25)';
+    controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+    controlText.style.fontSize = '16px';
+    controlText.style.lineHeight = '38px';
+    controlText.style.paddingLeft = '5px';
+    controlText.style.paddingRight = '5px';
+    controlText.innerHTML = 'Center Map';
+    controlUI.appendChild(controlText);
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(getCoordinates);
+        } else {
+            alert("4");
+        }
+    }
+
+    function getCoordinates(position) {
+        pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+    }
+
+    // Setup the click event listeners: simply set the map to Chicago.
+    controlUI.addEventListener('click', function() {
+        getLocation();
+        map.setCenter(pos);
+    });
+
+}
+
 function initMap() {
     var mapDiv = document.getElementById('map');
     map = new google.maps.Map(mapDiv, {
         center: {lat: 38.8320, lng: -77.3116},
         zoom: 16
     });
+
+    // Create the DIV to hold the control and call the CenterControl()
+    // constructor passing in this DIV.
+    var centerControlDiv = document.createElement('div');
+    var centerControl = new CenterControl(centerControlDiv, map);
+
+    centerControlDiv.index = 1;
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
 
     // every time the map is clicked on,
     // a pin is dropped and the previous pin is removed
@@ -119,7 +182,7 @@ foodshareRef.on('child_removed', function(data) {
 var tags = {};
 var foodCount = 0;
 foodshareRef.on("child_added", function(data){
-    if(data.val().img) return;
+    if(!data.val().lat || !data.val().lng) return;
     // foodCountDB = data.numChildren();
     // console.log(foodCountDB);
 
